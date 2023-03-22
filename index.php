3dd1fd83e56c6590380c99fd99f26e3f947a52a3
Romain1180 <romain.wybo1@gmail.com>
2023-03-22 14:16:57 +0100

    test

--- index.php
+++ index.php
@@ -5,15 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>ProjetXML</title>
 </head>
 <body>
     <header>
     </header>
+
+    <!-- Container catégories -->
     <div class="container-categories">
         <h2>Catégories :</h2>
         <div class="card-categories">
-            
+            <div class="card card1">
+                <img src="./assets/img/Rectangle 164.png" alt="">
+                <h4>Sport</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="card card2">
+                <img src="" alt="">
+                <h4>France Politique</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="card card3">
+                <img src="" alt="">
+                <h4>Monde</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="card card4">
+                <img src="" alt="">
+                <h4>Culture</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
         </div>
     </div>
 </body>
